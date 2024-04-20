34aa1b08771b8a5dd7dc0666a83549bf10404905
Suryakant <skant758@gmail.com>
2024-04-20 14:08:53 +0530

    created settings page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import InfoBar from "@/components/infobar";
 import Sidebar from "@/components/sidebar";
 import React from "react";
 
@@ -7,7 +8,10 @@ const Layout = ({ children }: Props) => {
 	return (
 		<div className='flex overflow-hidden h-screen'>
 			<Sidebar />
-			<div className='w-full'>{children}</div>
+			<div className='w-full'>
+				<InfoBar />
+				{children}
+			</div>
 		</div>
 	);
 };
