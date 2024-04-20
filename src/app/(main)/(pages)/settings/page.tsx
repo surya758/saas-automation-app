334158e395359489fc75ee8d1eee7733f505aa71
Suryakant <skant758@gmail.com>
2024-04-20 16:52:00 +0530

    added middleware

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -5,6 +5,52 @@ import ProfilePicture from "./_components/profile-picture";
 type Props = {};
 
 const Settings = (props: Props) => {
+	// const authUser = await currentUser()
+	// if (!authUser) return null
+
+	// const user = await db.user.findUnique({ where: { clerkId: authUser.id } })
+	// const removeProfileImage = async () => {
+	// 	"use server";
+	// 	const response = await db.user.update({
+	// 		where: {
+	// 			clerkId: authUser.id,
+	// 		},
+	// 		data: {
+	// 			profileImage: "",
+	// 		},
+	// 	});
+	// 	return response;
+	// };
+
+	// const uploadProfileImage = async (image: string) => {
+	//   'use server'
+	//   const id = authUser.id
+	//   const response = await db.user.update({
+	//     where: {
+	//       clerkId: id,
+	//     },
+	//     data: {
+	//       profileImage: image,
+	//     },
+	//   })
+
+	//   return response
+	// }
+
+	// const updateUserInfo = async (name: string) => {
+	//   'use server'
+
+	//   const updateUser = await db.user.update({
+	//     where: {
+	//       clerkId: authUser.id,
+	//     },
+	//     data: {
+	//       name,
+	//     },
+	//   })
+	//   return updateUser
+	// }
+
 	return (
 		<div className='flex flex-col gap-4'>
 			<h1 className='sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg'>
@@ -15,11 +61,11 @@ const Settings = (props: Props) => {
 					<h2 className='text-2xl font-bold'>User Profile</h2>
 					<p className='text-base text-white/50'>Add or update your information</p>
 				</div>
-				<ProfilePicture
-				// onDelete={removeProfileImage}
-				// userImage={user?.profileImage || ""}
-				// onUpload={uploadProfileImage}
-				/>
+				{/* <ProfilePicture
+					onDelete={removeProfileImage}
+					userImage={user?.profileImage || ""}
+					onUpload={uploadProfileImage}
+				></ProfilePicture> */}
 				<ProfileForm />
 			</div>
 		</div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const font = DM_Sans({ subsets: ["latin"] });
 
@@ -16,12 +17,19 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
-			<body className={font.className}>
-				<ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
-					{children}
-				</ThemeProvider>
-			</body>
-		</html>
+		<ClerkProvider>
+			<html lang='en'>
+				<body className={font.className}>
+					<ThemeProvider
+						attribute='class'
+						defaultTheme='dark'
+						enableSystem
+						disableTransitionOnChange
+					>
+						{children}
+					</ThemeProvider>
+				</body>
+			</html>
+		</ClerkProvider>
 	);
 }
