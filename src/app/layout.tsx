b1c09113f3fb50165ba27ea276ff9b8d1bac081c
Suryakant <skant758@gmail.com>
2024-04-21 23:10:28 +0530

    publishable key error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+		<ClerkProvider publishableKey='pk_test_dG91Z2gtbWFudGlzLTEwLmNsZXJrLmFjY291bnRzLmRldiQ'>
 			<html lang='en'>
 				<body className={font.className}>
 					<ThemeProvider
