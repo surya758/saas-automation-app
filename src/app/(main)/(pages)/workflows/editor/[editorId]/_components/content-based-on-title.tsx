462aeff1a122614aa22d0d06f031174af60e23c2
Suryakant <skant758@gmail.com>
2024-04-21 12:55:06 +0530

    wrote google file details component

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_components/content-based-on-title.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/content-based-on-title.tsx
@@ -1,11 +1,12 @@
 import { AccordionContent } from "@/components/ui/accordion";
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { onContentChange } from "@/lib/editor-utils";
 import { nodeMapper } from "@/lib/types";
 import { ConnectionProviderProps } from "@/providers/connections-provider";
 import { EditorState } from "@/providers/editor-provider";
 import React from "react";
+import GoogleFileDetails from "./google-file-details";
 
 export interface Option {
 	value: string;
@@ -74,6 +75,26 @@ const ContentBasedOnTitle = ({
 						type='text'
 						value={nodeConnectionType.content}
 						onChange={(event) => onContentChange(nodeConnection, title, event)}
+					/>
+
+					{JSON.stringify(file) !== "{}" && title !== "Google Drive" && (
+						<Card className='w-full'>
+							<CardContent className='px-2 py-3'>
+								<div className='flex flex-col gap-4'>
+									<CardDescription>Drive File</CardDescription>
+									<div className='flex flex-wrap gap-2'>
+										<GoogleFileDetails nodeConnection={nodeConnection} title={title} gFile={file} />
+									</div>
+								</div>
+							</CardContent>
+						</Card>
+					)}
+					{title === "Google Drive" && <GoogleDriveFiles />}
+					<ActionButton
+						currentService={title}
+						nodeConnection={nodeConnection}
+						channels={selectedSlackChannels}
+						setChannels={setSelectedSlackChannels}
 					/>
 				</div>
 			</Card>
