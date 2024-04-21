82ac799ed8874f4a2e42fad11a8a02086a2995d9
Suryakant <skant758@gmail.com>
2024-04-21 13:39:20 +0530

    fixed small error and bugs

--- src/app/(main)/(pages)/workflows/_components/workflow.tsx
+++ src/app/(main)/(pages)/workflows/_components/workflow.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import Image from "next/image";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
+import { onFlowPublish } from "../_actions/workflow-connections";
+import { toast } from "sonner";
 
 type Props = {
 	name: string;
@@ -13,6 +15,11 @@ type Props = {
 };
 
 const Workflow = ({ description, id, name, publish }: Props) => {
+	const onPublishFlow = async (event: any) => {
+		const response = await onFlowPublish(id, event.target.ariaChecked === "false");
+		if (response) toast.message(response);
+	};
+
 	return (
 		<Card className='flex w-full items-center justify-between'>
 			<CardHeader className='flex flex-col gap-4'>
